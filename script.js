39e9f84fc2e5c1973a091d7fdffa80f93ba41a4d
shaveushaveu <67775266+shaveushaveu@users.noreply.github.com>
2020-07-05 14:35:24 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+
+  // #で始まるアンカーをクリックした場合に処理
+  $('header a[href^=#]').click(function(){
+    // 移動先を50px上にずらす
+    var adjust = 50;
+    // スクロールの速度
+    var speed = 400; // ミリ秒
+    // アンカーの値取得
+    var href= $(this).attr("href");
+    // 移動先を取得
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    // 移動先を調整
+    var position = target.offset().top - adjust;
+    // スムーススクロール
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+
+});
